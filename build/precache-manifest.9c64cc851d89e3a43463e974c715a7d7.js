09a9ae1c77a3cfa8430b37d9522a6069ff6650d0
tomgastall <tomgastall@gmail.com>
2020-09-10 12:40:13 -0700

    commi

---
+++ build/precache-manifest.9c64cc851d89e3a43463e974c715a7d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e4033965b2ce0f752b0dff53bc3c22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71d894d80f0f45ff6443",
+    "url": "/static/css/main.ecd8849b.chunk.css"
+  },
+  {
+    "revision": "7d30a23edefa840d2da7",
+    "url": "/static/js/2.4f48ed68.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.4f48ed68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71d894d80f0f45ff6443",
+    "url": "/static/js/main.1b5ac589.chunk.js"
+  },
+  {
+    "revision": "ca731ebac3516acacce8",
+    "url": "/static/js/runtime-main.05f75025.js"
+  }
+]);
